f441b0ea5b08c257e054e75762e0e10e35357233
Hieu Tran <hieutm211@gmail.com>
2022-05-01 15:32:49 +0700

    fix (viet-bai): fix css for editor command menu

--- pages/viet-bai.js
+++ pages/viet-bai.js
@@ -65,144 +65,148 @@ export default function WritePost() {
         }
       })
       .catch((error) => {
-        alert(error);
+        console.error(error.code);
       });
   };
 
   return (
     <>
-      <Flex
-        alignItems="baseline"
-        justifyContent="space-between"
-        mx="auto"
-        px={{ base: "4", md: "0" }}
-        w="full"
-        maxW="container.sm"
-      >
-        <Flex flexDirection="column" gap="3" py="8" w="full">
-          <Flex
-            alignItems="center"
-            gap="3"
-            color="gray.400"
-            _hover={{ color: "gray.600" }}
-          >
-            <Button
-              variant="ghost"
-              fontSize="xl"
-              onClick={() => {
-                console.log(window.history.length);
-                if (window.history.length > 2) router.back();
-                else router.push("/");
-              }}
+      <Box h="100vh" position="relative">
+        <Flex
+          alignItems="baseline"
+          justifyContent="space-between"
+          mx="auto"
+          px="6"
+          w="full"
+          maxW="container.sm"
+          overflow="auto"
+        >
+          <Flex flexDirection="column" gap="3" py="8" w="full">
+            <Flex
+              alignItems="center"
+              gap="3"
+              color="gray.400"
+              _hover={{ color: "gray.600" }}
             >
-              &larr;
-            </Button>
-          </Flex>
-          <Flex alignItems="center" justifyContent="space-between" mb="6">
-            <Box
-              as="h1"
-              fontSize="2xl"
-              fontWeight="semibold"
-              color="gray.600"
-              fontFamily="heading"
-            >
-              Tạo bài viết mới
-            </Box>
-            <Button disabled={!title || !content} onClick={handlePost}>
-              Đăng bài
-            </Button>
-          </Flex>
-          <Input
-            placeholder="Tiêu đề bài viết"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          {(!content || content === "\\\n") && (
-            <div>
-              <Box
-                as="input"
-                display="none"
-                ref={uploadInputRef}
-                type="file"
-                accept="text/plain, text/markdown"
-                onChange={(e) => {
-                  if (!e.target.files.length) {
-                    return;
-                  }
-                  const [file] = e.target.files;
-                  if (!file) {
-                    return;
-                  }
-
-                  setTitle(file.name.replace(/\.[^/.]+$/, ""));
-
-                  const reader = new FileReader();
-                  reader.addEventListener(
-                    "load",
-                    () => {
-                      setDefaultContent(reader.result);
-                      setContent(reader.result);
-                    },
-                    false
-                  );
-                  reader.readAsText(file);
+              <Button
+                variant="ghost"
+                fontSize="xl"
+                onClick={() => {
+                  console.log(window.history.length);
+                  if (window.history.length > 2) router.back();
+                  else router.push("/");
                 }}
-              />
-              {isImport && (
-                <Input
-                  mb="2.5"
-                  placeholder="https://example.com/duong-dan-den-file-markdown.md"
-                  value={importURL}
-                  onChange={(e) => setImportURL(e.target.value)}
+              >
+                &larr;
+              </Button>
+            </Flex>
+            <Flex alignItems="center" justifyContent="space-between" mb="6">
+              <Box
+                as="h1"
+                fontSize="2xl"
+                fontWeight="semibold"
+                color="gray.600"
+                fontFamily="heading"
+              >
+                Tạo bài viết mới
+              </Box>
+              <Button disabled={!title || !content} onClick={handlePost}>
+                Đăng bài
+              </Button>
+            </Flex>
+            <Input
+              placeholder="Tiêu đề bài viết"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            {(!content || content === "\\\n") && (
+              <div>
+                <Box
+                  as="input"
+                  display="none"
+                  ref={uploadInputRef}
+                  type="file"
+                  accept="text/plain, text/markdown"
+                  onChange={(e) => {
+                    if (!e.target.files.length) {
+                      return;
+                    }
+                    const [file] = e.target.files;
+                    if (!file) {
+                      return;
+                    }
+
+                    setTitle(file.name.replace(/\.[^/.]+$/, ""));
+
+                    const reader = new FileReader();
+                    reader.addEventListener(
+                      "load",
+                      () => {
+                        setDefaultContent(reader.result);
+                        setContent(reader.result);
+                      },
+                      false
+                    );
+                    reader.readAsText(file);
+                  }}
+                />
+                {isImport && (
+                  <Input
+                    mb="2.5"
+                    placeholder="https://example.com/duong-dan-den-file-markdown.md"
+                    value={importURL}
+                    onChange={(e) => setImportURL(e.target.value)}
+                  />
+                )}
+                <Flex flexWrap="wrap" gap="1" fontSize="sm" color="gray.500">
+                  <span>Bạn đã có bài viết?</span>
+                  <Box
+                    as="button"
+                    type="button"
+                    textDecoration="underline"
+                    className="underline"
+                    onClick={() => uploadInputRef.current.click()}
+                  >
+                    upload
+                  </Box>
+                  <span>hoặc</span>
+                  <Box
+                    as="button"
+                    type="button"
+                    textDecoration="underline"
+                    onClick={() => setIsImport((value) => !value)}
+                  >
+                    import
+                  </Box>
+                  <span>file của bạn.</span>
+                  <span>(chỉ hỗ trợ định dạng markdown)</span>
+                </Flex>
+              </div>
+            )}
+            <Flex
+              flexDirection="col"
+              pb="8"
+              mt="4"
+              gap={{ base: "4", md: "6" }}
+              lineHeight="tall"
+              color="gray.900"
+              fontSize="lg"
+              spellCheck="false"
+              sx={{ "& > div": { w: "full" } }}
+            >
+              {isImporting ? (
+                <div>loading...</div>
+              ) : (
+                <Editor
+                  placeholder={"Viết điều gì đó..."}
+                  defaultValue={defaultContent}
+                  onChange={(value) => setContent(value)}
                 />
               )}
-              <Flex gap="1" fontSize="sm" color="gray.500">
-                <span>Bạn đã có bài viết?</span>
-                <Box
-                  as="button"
-                  type="button"
-                  textDecoration="underline"
-                  className="underline"
-                  onClick={() => uploadInputRef.current.click()}
-                >
-                  upload
-                </Box>
-                <span>hoặc</span>
-                <Box
-                  as="button"
-                  type="button"
-                  textDecoration="underline"
-                  onClick={() => setIsImport((value) => !value)}
-                >
-                  import
-                </Box>
-                <span>file của bạn.</span>
-                <span>(chỉ hỗ trợ định dạng markdown)</span>
-              </Flex>
-            </div>
-          )}
-          <Flex
-            flexDirection="col"
-            pb="8"
-            mt="4"
-            gap={{ base: "4", md: "6" }}
-            lineHeight="tall"
-            color="gray.900"
-            fontSize="lg"
-            spellCheck="false"
-          >
-            {isImporting ? (
-              <div>loading...</div>
-            ) : (
-              <Editor
-                placeholder={"Viết điều gì đó..."}
-                defaultValue={defaultContent}
-                onChange={(value) => setContent(value)}
-              />
-            )}
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
+      </Box>
     </>
   );
 }
